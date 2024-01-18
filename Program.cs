910597b4e71cbc07550c351fb9929310285f3e0c
Prince Bhagat <83371230+hrprem16@users.noreply.github.com>
2024-01-18 16:36:42 +0530

    Solving using Switch Case

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace uc4
+{
+    class Program
+    {
+      
+
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int value = Convert.ToInt32(input);
+
+            switch(value){
+                case 1:
+                    Console.WriteLine("Employee Wages are" + (20 * 8));
+                    break;
+                case 2:
+                    Console.WriteLine("Employee Wages are " + (20 * 4));
+                    break;
+                default:
+                    Console.WriteLine("Employee Wages are" + 0);
+                    break;
+
+            }
+        }
+    }
+}
